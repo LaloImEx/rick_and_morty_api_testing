472aec6aff0371150499137d4ea2540f2ff920e6
= <angelangelangel23abril@gmail.com>
2026-02-18 10:08:42 -0600

    Add characters endpoint to main.js

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
-async function filterCharacters() {
-    
-    //Request
-    try{
-        response = await fetch("https://rickandmortyapi.com/api/character");
-        data = await response.json();
-    }catch(error){
-        console.log("Error inesperado: ", error);
-    }
-    
-    //Filter
-    const characters = data.results;
-    const alive = characters.filter(ch => ch.status === "Alive");
-    alive.forEach(ch => console.log(ch.name));
+const express = require("express");
+const app = express();
 
-    //Replace spaces
-    const result = {
-        results: alive.map(ch => ({
-        id: ch.id,
-        name: ch.name.replace(/ /g, "_"),
-        status: ch.status,
-        gender: ch.gender
-        }))
-    };
-
-    //Print modified names
-    console.log(JSON.stringify(result, null, 2));
+async function getCharacters() {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    return data.results;
 }
 
-filterCharacters();
+function fiter_by_status(characters, status) {
+  return characters.filter(chara => chara.status === status);
+}
+
+function replace_spaces(characters) {
+    return characters.map(chara => ({
+        id: chara.id,
+        name: chara.name.replace(/ /g, "_"),
+        status: chara.status,
+        gender: chara.gender
+  }));
+}
+
+app.get("/characters", async (request, response) => {
+  try {
+    const characters = await getCharacters();
+    const alive = fiter_by_status(characters, "Alive");
+    const modifiedNames = { modifiedNames: replace_spaces(alive) };
+    response.json(modifiedNames);
+  } catch (error) {
+    response.status(500).json({ error: "Error datils: ", detalle: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
