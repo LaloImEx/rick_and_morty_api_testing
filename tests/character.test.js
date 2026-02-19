e32cbfa138d869f215eb13d83cc03b77bc2bdb34
= <angelangelangel23abril@gmail.com>
2026-02-18 23:58:55 -0600

    Tests: add character endpoint tests

---
+++ tests/character.test.js
@@ -0,0 +1,52 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+describe("GET /characters", () => {
+
+  test("get characters", async () => {
+    const response = await request(app).get("/characters");
+    expect(response.status).toBe(200);
+  });
+
+  test("get characters with different page", async () => {
+    const response = await request(app).get("/characters/5");
+    expect(response.status).toBe(200);
+  });
+
+  test("get characters with impossible page", async () => {
+    const response = await request(app).get("/characters/80");
+    expect(response.status).toBe(500);
+  });
+
+  test("response contains array", async () => {
+    const response = await request(app).get("/characters");
+    expect(Array.isArray(response.body.results)).toBe(true);
+  });
+
+  test("response contains array with choosed page", async () => {
+    const response = await request(app).get("/characters/6");
+    expect(Array.isArray(response.body.results)).toBe(true);
+  });
+
+  test("only alive character", async () => {
+    const response = await request(app).get("/characters");
+    response.body.results.forEach(character => {
+      expect(character.status).toBe("Alive");
+    });
+  });
+
+  test("name without spaces", async () => {
+    const response = await request(app).get("/characters");
+    response.body.results.forEach(ch => {
+      expect(ch.name).not.toContain(" ");
+    });
+  });
+
+  test("names without spaces with choosen page ", async () => {
+    const response = await request(app).get("/characters/3");
+    response.body.results.forEach(character => {
+      expect(character.name).not.toContain(" ");
+    });
+  });
+
+});
